7c6c9372d70a74468cb34e009120e32a946c44cd
LuccaMeurer <luccameurer1@gmail.com>
2025-09-24 21:59:11 -0300

    Initial commit

---
+++ src/main/java/com/br/fiap/postech_fase3_historico/PostechFase3HistoricoApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech_fase3_historico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechFase3HistoricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechFase3HistoricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/postech_fase3_historico/PostechFase3HistoricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech_fase3_historico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostechFase3HistoricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
